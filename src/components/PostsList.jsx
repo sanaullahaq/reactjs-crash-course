7959f542e4dac9cfc6fc9e1ce0c8733829d4a283
Sanaulla Haq <sanaullahaq01@gmail.com>
2023-11-19 18:24:05 +0600

    LC

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,12 +2,36 @@ import Post from "./Post";
 import classes from "./PostsList.module.css";
 import NewPost from "./NewPost";
 import Modal from "./Modal";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function PostsList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false)
+
+  // useEffect() hook is used to minimize the side effect when the components executes.
+  // e.g. here when we fetchig the data and changing the state thus the fetching method will run again and cause a infite loop.
+  // but the useEffect() hook will be runed based on its dependency here it the empty [] array. so it will run once, does not matter how many times
+  // the components runs. the depency can be altered from any where,
+  // useEffect() hook function do not expect to return a promise, so async-await used in its inner function
+  useEffect(()=>{
+    async function fetchPosts(){
+      setIsFetching(true)
+      const response = await fetch('http://localhost:8080/posts') //default request type is 'GET'
+      const resData = await response.json()
+      setPosts(resData.posts)
+      setIsFetching(false)
+    }
+    fetchPosts()
+  }, [])
 
   function addPostHandler(postData) {
+    fetch('http://localhost:8080/posts', {
+      method: 'POST',
+      body: JSON.stringify(postData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
     setPosts((existingPosts) => [postData, ...existingPosts]);
     // inside the setPosts() we can diretly spread the list,
     // but it not sure that setPosts() will be called instantly whent he stage is changed,
@@ -24,17 +48,22 @@ function PostsList({ isPosting, onStopPosting }) {
         </Modal>
       )}
 
-      {posts.length > 0 && (
+      {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
             <Post key={post.body} text={post.body} author={post.author} />
           ))}
         </ul>
       )}
-      {posts.length === 0 && (
+      {!isFetching && posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>There are no posts yet.</h2>
           <p>Start adding some!</p>
+        </div>
+      )}
+      {isFetching && (
+        <div style={{ textAlign: 'center', color: 'white' }}>
+          <p>Loading posts...</p>
         </div>
       )}
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
 import App from './App'
 import './index.css'
+import NewPost from './components/NewPost'
+
+const router = createBrowserRouter([
+  { path: '/', element: <App /> },
+  { path: '/create-posts', element: <NewPost /> },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
